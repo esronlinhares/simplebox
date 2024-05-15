2a56c61e6fd51b15b53a587c05d16a21cb132f55
Esron <linhares.esron@gmail.com>
2024-05-15 10:55:42 -0300

    revert 1

--- src/pages/ruas/index.js
+++ src/pages/ruas/index.js
@@ -13,6 +13,8 @@ import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import Spinner from 'react-native-loading-spinner-overlay';
 
+//teste
+
 export default function Ruas() {
   const [ruas, setRuas] = useState([]);
   const [ruaSelecionada, setRuaSelecionada] = useState(null);
